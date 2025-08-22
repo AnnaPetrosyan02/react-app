96039fc529d10999d4fc530b431a7ad2db1f9b1f
Anna Petrosyan <annapetrosyan@MacBook-Air-Anna.local>
2025-08-23 00:01:20 +0200

    Exercise 2

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+/*function App() {
   const tasks = ['Помыть посуду', 'Сделать домашнее задание', 'Прочитать книгу'];
   return (
     <div id='root'>
@@ -15,6 +15,46 @@ function App() {
       </ul>
     </div>
   );
+}*/
+
+/*
+1. Создай компонент `UserCard`, который принимает через пропсы имя пользователя (`name`) и его возраст (`age`).
+
+2. Используй компонент `UserCard` в `App`, чтобы отобразить трёх разных пользователей. Пример вызова:
+<UserCard name="Мария" age={25} /> 
+*/
+
+function UserCard({name, age}){
+  return (
+    <div className='userCard'>
+      <h3>User: {name}</h3>
+      <p>age: {age}</p>
+    </div>
+  )
+}
+
+function App(){
+  /* return (
+    <div>
+      <UserCard name="Anna" age={22} />
+      <UserCard name="Mane" age={21} />
+      <UserCard name="Irene" age={19} />
+    </div>
+  )*/
+
+    const users =[
+      {name: 'Anna', age:22 },
+      {name: 'Mane', age:21 },
+      {name: 'Irene', age:19 }
+    ]
+
+    return (
+      <div>
+        {users.map((user, index) => (
+          <UserCard key={index} name={user.name} age={user.age}/>
+        ))}
+      </div>
+    )
 }
 
 
